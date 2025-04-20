c767dd70bedc00cd79108c814693ae1a0a29d1c0
AlexiusMD <alexiusmd2004@gmail.com>
2025-04-20 19:50:37 -0300

    feat: start main menu

--- src/main.c
+++ src/main.c
@@ -1,9 +1,61 @@
 #include "../includes/asm_compiler.h"
+#include <stdio.h>
+
+void printMenuOptions();
+void getArrivalAndPeriod(int* arrival_time, int* period);
+void CLIMenu();
 
 int main(int argc, char* argv[]) {
-    const char* filepath = "programs/prog2.txt";
-    const char* filepath2 = "programs/prog1.txt";
-    create_task(filepath, 0, 20);
-    create_task(filepath2, 5, 20);
+    CLIMenu();
     return 0;
+}
+
+void printMenuOptions() {
+    printf("Escolha qual programa quer carregar:\n");
+    printf("====================================\n");
+    printf("1. Programa 1\n");
+    printf("2. Programa 2 \n");
+    printf("3. Iniciar execução\n");
+}
+
+void CLIMenu() {
+    int choice = 0;
+    int arrival_time = 0;
+    int period = 0;
+
+    while(1) {
+        printMenuOptions();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                getArrivalAndPeriod(&arrival_time, &period);
+                create_task("programs/prog1.txt", arrival_time, period);
+                break;
+            case 2:
+                getArrivalAndPeriod(&arrival_time, &period);
+                create_task("programs/prog2.txt", arrival_time, period);
+                break;
+            case 3:
+                return;
+            default:
+                printf("\nEscolha uma opção válida do menu!\n\n");
+                break;
+        }
+    }
+}
+
+void getArrivalAndPeriod(int* arrival_time, int* period) {
+    do {
+        printf("Escolha um tempo de chegada para a tarefa. [>= 0]\n");
+        scanf("%d", arrival_time);
+        if(*arrival_time < 0) printf("\nEscolha um valor válido para a chegada!\n\n");
+    } while (*arrival_time < 0);
+
+    do {
+        printf("Escolha um período para a tarefa. [> 0]\n");
+        scanf("%d", period);
+
+        if(*period <= 0) printf("\nEscolha um valor válido para o período!\n\n");
+    } while (*period <= 0);
 }
